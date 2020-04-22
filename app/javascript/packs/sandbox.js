8a5bcbd62814014272dc666dd7227898333edd18
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-04-22 11:44:22 +0900

    createjs.Shape.setTransform を使用するよう修正
    また、表示範囲が canvas タグの大きさまでということを示すために myCanvas の width と height を修正

--- app/javascript/packs/sandbox.js
+++ app/javascript/packs/sandbox.js
@@ -4,14 +4,13 @@ window.addEventListener("load", init);
 
 function init() {
     // Stageオブジェクトを作成します
-    var stage = new CreateJs.Stage("myCanvas");
+    const stage = new CreateJs.Stage("myCanvas");
 
     // 円を作成します
-    var shape = new CreateJs.Shape();
+    const shape = new CreateJs.Shape();
     shape.graphics.beginFill("DarkRed"); // 赤色で描画するように設定
-    shape.graphics.drawCircle(0, 0, 100); //半径 100px の円を描画
-    shape.x = 200; // X 座標 200px の位置に配置
-    shape.y = 200; // Y 座標 200px の位置に配置
+    shape.graphics.drawCircle(0, 0, 100); // 中心座標(x, y)から半径 100px の円を描画
+    shape.setTransform(200, 200, 2, 1.5, 45); // X 座標 200px , Y 座標 200px の位置に配置, scale は倍率を指定, skew は歪み, reg は registration
     stage.addChild(shape); // 表示リストに追加
 
     // Stageの描画を更新します
