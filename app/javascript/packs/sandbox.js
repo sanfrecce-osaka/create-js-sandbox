08be495eaa99661359f962e3f429cba33d366b3f
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-04-22 16:05:33 +0900

    レイヤー別にCreateJs.Container を追加
    各 Shape は適当に各 Container に振り分け

--- app/javascript/packs/sandbox.js
+++ app/javascript/packs/sandbox.js
@@ -6,45 +6,34 @@ function init() {
     // Stageオブジェクトを作成します
     const stage = new CreateJs.Stage("myCanvas");
 
-    // 円を作成します
-    drawCircle(stage);
+    const bottomLayer = new CreateJs.Container();
+    bottomLayer.setTransform(0, 0);
+    bottomLayer.addChild(drawCircle(), drawRect());
 
-    // 四角形を作成します
-    drawRect(stage);
+    const topLayer = new CreateJs.Container();
+    topLayer.setTransform(0, 0);
+    topLayer.addChild(drawPentagon(), drawPentagonOutline(), drawLine(), drawText())
 
-    // 五角形を作成します
-    drawPentagon(stage);
-
-    // 白抜きで五角形を作成します
-    drawPentagonOutline(stage);
-
-    // 線を作成します
-    drawLine(stage);
-
-    // テキストを追加します
-    drawText(stage);
-
+    stage.addChild(bottomLayer, topLayer);
     stage.update();
 }
 
-const drawCircle = (stage) => {
+const drawCircle = () => {
     const circle = new CreateJs.Shape();
     circle.graphics.beginFill("darkred"); // 赤色で描画するように設定
     circle.graphics.drawCircle(0, 0, 100); // 中心座標(x, y)から半径 100px の円を描画
     circle.setTransform(200, 200, 2, 1.5, 45); // X 座標 200px , Y 座標 200px の位置に配置, scale は倍率を指定, skew は歪み, reg は registration
-
-    stage.addChild(circle); // 表示リストに追加
+    return circle;
 }
 
-const drawRect = (stage) => {
+const drawRect = () => {
     const rect = new CreateJs.Shape();
     rect.graphics.beginFill("#0000FF");
     rect.graphics.drawRect(0, 0, 100, 100);
-
-    stage.addChild(rect);
+    return rect
 }
 
-const drawPentagon = (stage) => {
+const drawPentagon = () => {
     const pentagon = new CreateJs.Shape();
     pentagon.graphics.beginFill("green")
     pentagon.graphics.moveTo(100, 300)
@@ -53,11 +42,10 @@ const drawPentagon = (stage) => {
                      .lineTo(100, 500)
                      .lineTo(50, 350);
     pentagon.graphics.endFill();
-
-    stage.addChild(pentagon);
+    return pentagon
 }
 
-const drawPentagonOutline = (stage) => {
+const drawPentagonOutline = () => {
     const outline = new CreateJs.Shape();
     outline.graphics.setStrokeStyle(5)
     outline.graphics.beginStroke("red");
@@ -67,11 +55,10 @@ const drawPentagonOutline = (stage) => {
                     .lineTo(400, 500)
                     .lineTo(350, 350)
                     .lineTo(400, 300);
-
-    stage.addChild(outline);
+    return outline
 }
 
-const drawLine = (stage) => {
+const drawLine = () => {
     const line = new CreateJs.Shape();
     line.graphics.setStrokeStyle(5)
     line.graphics.beginStroke("black");
@@ -79,13 +66,11 @@ const drawLine = (stage) => {
                  .lineTo(400, 100)
                  .lineTo(450, 100);
     line.graphics.endStroke();
-
-    stage.addChild(line);
+    return line;
 }
 
-const drawText = (stage) => {
+const drawText = () => {
     const text = new CreateJs.Text("create js sandbox", "10px sans-serif", "orange");
     text.setTransform(250, 50, 5, 5, 90); // scale は倍率を指定
-
-    stage.addChild(text);
+    return text;
 }
