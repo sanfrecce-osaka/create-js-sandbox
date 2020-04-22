7f38345134d89eb6fd8df455a4bc08b4b4b30211
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-04-22 13:16:34 +0900

    四角形の描画処理を追加
    ### その他の修正
    - 円の描画処理を関数に切り出し
      - update は1回でいいが、update で描画が更新されるということを伝えるためこうしている
    - add された children の コンソールへの出力を追加

--- app/javascript/packs/sandbox.js
+++ app/javascript/packs/sandbox.js
@@ -7,12 +7,31 @@ function init() {
     const stage = new CreateJs.Stage("myCanvas");
 
     // 円を作成します
-    const shape = new CreateJs.Shape();
-    shape.graphics.beginFill("DarkRed"); // 赤色で描画するように設定
-    shape.graphics.drawCircle(0, 0, 100); // 中心座標(x, y)から半径 100px の円を描画
-    shape.setTransform(200, 200, 2, 1.5, 45); // X 座標 200px , Y 座標 200px の位置に配置, scale は倍率を指定, skew は歪み, reg は registration
-    stage.addChild(shape); // 表示リストに追加
+    drawCircle(stage)
+
+    // 四角形を作成します
+    drawRect(stage)
+}
+
+const drawCircle = (stage) => {
+    const circle = new CreateJs.Shape();
+    circle.graphics.beginFill("darkred"); // 赤色で描画するように設定
+    circle.graphics.drawCircle(0, 0, 100); // 中心座標(x, y)から半径 100px の円を描画
+    circle.setTransform(200, 200, 2, 1.5, 45); // X 座標 200px , Y 座標 200px の位置に配置, scale は倍率を指定, skew は歪み, reg は registration
+    stage.addChild(circle); // 表示リストに追加
+    console.log(stage.children);
 
     // Stageの描画を更新します
+    stage.update();
+}
+
+const drawRect = (stage) => {
+    const rect = new CreateJs.Shape();
+    rect.graphics.beginFill("#0000FF");
+    rect.graphics.drawRect(0, 0, 100, 100);
+
+    stage.addChild(rect);
+    console.log(stage.children);
+
     stage.update();
 }
